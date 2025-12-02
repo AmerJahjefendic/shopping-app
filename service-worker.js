59c7cabc1faa7b5b70c160e92f4e5e880b01da2a
AmerJahjefendic <119766646+AmerJahjefendic@users.noreply.github.com>
2025-12-02 22:05:50 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,47 @@
+const CACHE_NAME = "shopping-app-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  "icons/icon-1024.png"
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('shopping-cache').then(cache => {
-      return cache.addAll(['/', '/index.html', '/manifest.json']);
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate SW
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch handler
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          caches.match("index.html")
+        )
+      );
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
-});
